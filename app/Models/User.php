95787c2dcbb1981a155ce208ed725596dd281adf
Amir Daneshkar <amir.daneshkar@hotmail.com>
2020-11-09 20:35:42 +0330

    Add Role Model and migration and pivot
     Create Role model
     Create role migration
     Create role_users pivot table migration
     Add the relationship for role and user to each class

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * undocumented function
+     *
+     * @return void
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
