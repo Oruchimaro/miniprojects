b8b23e3a3d0fc79082e4d62e4f4f69280fa17183
Amir Daneshkar <amir.daneshkar@hotmail.com>
2020-11-14 19:45:41 +0330

    Create databse seeder and rename model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Kyslik\ColumnSortable\Sortable;
 
-class product extends Model
+class Product extends Model
 {
     use HasFactory;
     use Sortable;
