f3724ac689b2794858eb106fd35edb650e169af5
Amir Daneshkar <amir.daneshkar@hotmail.com>
2022-02-26 14:25:18 +0330

    added the converter job

---
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Video;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use PhpOffice\PhpSpreadsheet\Helper\Dimension;
+
+class ConvertVideoForStreaming implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $video;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Video $video)
+    {
+        $this->video = $video;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $lowBitrateFormat = (new X264('libmp3lame', 'libx264'))->setKiloBitrate(500);
+        $converted_name = $this->getCleanFileName($this->video->path);
+
+        FFMpeg::fromDisk($this->video->disk)
+            ->open($this->video->path)
+            ->addFilter(function($filters){
+                $filters->resize(new Dimension(960, 540));
+            })->export()
+            ->toDisk('public')
+            ->inFormat($lowBitrateFormat)
+            ->save($converted_name);
+
+
+        $this->video->update([
+            'converted_for_streaming_at' => Carbon::now(),
+            'processed' => true,
+            'stream_path' => $converted_name
+        ]);
+    }
+
+
+    private function getCleanFileName($filename){
+        return preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename) . '.mp4';
+    }
+}
